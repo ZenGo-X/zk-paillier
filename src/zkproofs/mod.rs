b7b696e4c7f182ab54d6908d9895e80d8f455bda
omershlo <omer.shlomovits@gmail.com>
2019-02-09 00:50:45 +0200

    removing proof of equal message

--- src/zkproofs/mod.rs
+++ src/zkproofs/mod.rs
@@ -35,8 +35,6 @@ pub use self::range_proof::Proof;
 mod range_proof_ni;
 pub use self::range_proof_ni::RangeProofError;
 pub use self::range_proof_ni::RangeProofNi;
-mod equal_message_proof;
-pub use self::equal_message_proof::{NISigmaProof, Proof as EqualMessageProof, Statement, Witness};
 use curv::BigInt;
 use ring::digest::{Context, SHA256};
 use std::borrow::Borrow;
